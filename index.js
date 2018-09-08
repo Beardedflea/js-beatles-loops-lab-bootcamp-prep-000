c4f502740da40bd3776678a2f1a9e32e51547abf
Linzell Godfrey <linzell.godfrey@live.co.uk>
2018-09-08 12:31:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var theBeatlesPlay = new Array();
+  for (let i = 0; i < 4; i++){
+    theBeatlesPlay.push(musicians[i]+ " plays " + instruments[i]);
+  }
+  return theBeatlesPlay;
+}
+
+function johnLennonFacts(facts){
+  let i = 0;
+  while(i < facts.length){
+    facts.push([i] + "!!!");
+    i++
+  }
+  return facts;
+}
